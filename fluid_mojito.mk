e2cc0c0f72556b9b137f02e462c03a23ab155a75
Anush02198 <Anush.4376@gmail.com>
2021-10-15 02:24:46 +0530

    mojito:Use Coral October Fingerprint
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- fluid_mojito.mk
+++ fluid_mojito.mk
@@ -23,6 +23,13 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="mojito" \
+    PRODUCT_NAME="mojito" \
+    PRIVATE_BUILD_DESC="coral-user 11 RQ3A.211001.001 7641976 release-keys"
+
+BUILD_FINGERPRINT :="google/coral/coral:11/RQ3A.211001.001/7641976:user/release-keys"
+
 # #Gapps
 # TARGET_GAPPS_ARCH = arm64
 #
